8b27e47531e77f941c7fe01c50662662f07c3dbe
raincoats <raincoats@users.noreply.github.com>
2016-03-12 19:51:57 +1100

    made a file xor-er xor.c, the old xor is now xor-string.c

--- xor.c
+++ xor.c
@@ -1,46 +1,52 @@
-/*
- * xor.c
- * ~~~~~
- *
- * tiny string xor'er
- * by @reptar_xl
- *
- * 7 mar 2016
- *
- * i don't know c very well, as you can tell...
- *
-*/
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3) {
-		fprintf(stderr, 
-			"usage: %s <word> <word>\n"
-			"simple xor calculator by @reptar_xl\n"
-			"note: strings must be the same length... :/\n"
-			, argv[0]);
-		return 4;
+	if (argc < 2) {
+		fprintf(stderr, "%s: need an filename\n", argv[0]);
+		return 1;
+	}
+	else if (argc < 3) {
+		fprintf(stderr, "%s: need a byte (like ff, or 0a, or whatev)\n", argv[0]);
+		return 1;
 	}
 
-	char *one = argv[1];
-	char *two = argv[2];
-	int xor;
 
-	while (*one && *two) {
-		xor = (*one++ ^ *two++);
+	//int cipher = 0xff;
+	int cipher = strtol(argv[2], NULL, 16);
+	//int byte = *argv[1];
+	char *filename = argv[1];
+	char *outfile;
+	FILE *fp = fopen(filename, "rb");
 
-		// if outside the printable ascii range
-		if ((xor < 0x20) || (xor > 0x7e))
-			printf("\\x%.2x", xor);
-		else
-			putc(xor, stdout);
+	if ((cipher < 0x00) || (cipher > 0xff)) {
+		fprintf(stderr, "%s: “%#2x”: must be between 0x00 and 0xff\n", 
+			argv[0], cipher);
+		return 1;
 	}
 
-	if (isatty(1) && isatty(2) && isatty(0))
-		putchar(0x0a);
+	if (! fp) {
+		fprintf(stderr, "%s: could not open file “%s”\n", argv[0], filename);
+		return 1;
+	}
 
+ 	fprintf(stderr, "%s: cipher is 0x%02x\n", argv[0], cipher);
+
+	int c;
+    while ((c = fgetc(fp)) != EOF) { // standard C I/O file reading loop
+       putchar(c ^ cipher);
+    }
+
+    fclose(fp);
+
+//	printf("%.2x", cipher ^ byte);
+//	putchar(0x0a);
 	return 0;
 }
+
