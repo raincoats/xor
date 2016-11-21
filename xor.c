b9fde19b8161c5e72be3c4c5371bf7f360021d59
raincoats <raincoats@users.noreply.github.com>
2016-11-21 18:13:58 +1100

    big internal changes
    getopt added, version bumped, bytes must now be specified with -c, null bytes can now be ignored with a flag rather than a compile-time definition

--- xor.c
+++ xor.c
@@ -1,74 +1,118 @@
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <inttypes.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/uio.h>
 #include <unistd.h>
 #include <err.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-#ifdef DONT_XOR_NULLS
+uint8_t cipher = 0xff;
+bool cipher_given = false;
+bool xor_nulls = false;
+
+
+void usage(char *argv0)
+{
+	dprintf(2,
+		"xor - performs an xor on stdin and a byte given with -c\n"
+		"usage: %s [OPTIONS] -c byte\n"
+		"   -c byte   the byte (in hex) to xor stdin with\n"
+		"   -0        ignore null bytes in input\n"
+		"   -V        version\n"
+		"   -h        help\n"
+		"\n"
+		"example: cat /etc/passwd | %s -c ff\n"
+		, argv0, argv0);
+	exit(2);
+}
+
+void version()
+{
+	dprintf(2, "xor - version 0.2\n"
+		       "https://github.com/raincoats/\n");
+	exit(2);
+}
+
 /*
- *  the original intention of this program was to obfusticate binaries. since
- *  binaries seem to have a whole bunch of \0's at the bottom, and anything
- *  xor'd by 0 is itself, it would just print the xor key.
- *
- *  the purpose of this program has changed since then, to be a general xor-er.
- *  so now it xor's every character, unless compiled with -DDONT_XOR_NULLS.
- *
- *  TODO: copy paste some getopt example and make it work
- *
+ *   this is where the magic happens
+ *   adapted from glibc's memfrob()
  */
-// stolen from glibc's memfrob()
-void *xor (void *s, size_t n, unsigned int x)
+void *xor (void *s, size_t n, uint8_t x)
 {
-	unsigned char *p = (unsigned char *) s;
-	while (n-- > 0) {
-		if (! ((*p == x) || (*p == 0))) {
-			*p ^= x;
+	unsigned char *p = (unsigned char *)s;
+
+	/*
+	 *   since anything xor'd by 0 is itself, you may want to ignore null bytes
+	 *   when xor'ing, as it would just print the xor key. so you can pass the
+	 *   -0 flag to do this.
+	 */
+	if (xor_nulls == true)
+	{
+		while (n --> 0) {
+			if (! ((*p == x) || (*p == 0))) {
+				*p ^= x;
+			}
+			p++;
 		}
-		p++;
 	}
+	else
+	{
+		while (n --> 0)
+			*p++ ^= x;
+	}
+
 	return s;
 }
-#else
-void *xor (void *s, size_t n, unsigned int x)
+
+uint8_t arg_to_cipher(char *arg)
 {
-	unsigned char *p = (unsigned char *) s;
+	if (arg == NULL)
+		return false;
 
-	while (n-- > 0)
-		*p++ ^= x;
+	uintmax_t c = (uintmax_t)strtol(optarg, NULL, 16);;
 
-	return s;
+	if (errno == EINVAL) {
+		err(errno, "could not convert '%s' to hex", arg);
+		exit(1);
+	}
+	if (c > UINT8_MAX) {
+		dprintf(2, "'0x%s': is too big. must be between 0x00 and 0xFF\n", arg);
+		exit(1);
+	}
+
+	cipher = (uint8_t)c;
+	dprintf(2, "using cipher '0x%02x'\n", cipher);
+	cipher_given = true;
+	return true;
 }
-#endif
 
+
+// taking this out of the main loop is okay, right?
+void do_getopts(int argc, char **argv)
+{
+	int o;
+
+	while ((o = getopt(argc, argv, "hV0c:")) != -1) {
+		switch(o) {
+			case 'h': usage(argv[0]);        break;
+			case 'V': version();             break;
+			case '0': xor_nulls = true;      break;
+			case 'c': arg_to_cipher(optarg); break;
+		}
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	// 4096 * 8 is fast. idk why. is it too much?
-	unsigned int bif_size = 4096 * 8;
-	unsigned char biffer[bif_size];
-	unsigned int cipher = 0xff;
-	unsigned int size = 0;
+	int bif_size = 8192;
+	char biffer[bif_size];
+	int size = 0;
+	
+	do_getopts(argc, argv);
 
-	if (argc < 2) {
+	if (! cipher_given)
 		dprintf(2, "%s: no cipher provided. using 0x%02x\n", argv[0], cipher);
-	}
-	else {
-		unsigned int e = (cipher = (unsigned int)strtol(argv[1], NULL, 16));
-		if ((! e) && (cipher != 0)) {
-			err(e, "could not convert %s to hex", argv[1]);
-			return 1;
-		}
-		else if (cipher > 0xFF) {
-			dprintf(2, "%s: cipher 0x%02X is out of range.\n", argv[0], cipher);
-			dprintf(2, "%s: must be between 0x00 and 0xFF.\n", argv[0]);
-			return 1;
-		}
-	}
 
 	while ((size = read(0, biffer, bif_size)) > 0)
 	{
@@ -76,6 +120,8 @@ int main(int argc, char *argv[])
 		write(1, biffer, size);
 	}
 
+	if (isatty(1))
+		putchar('\n');
 
 	return 0;
 }
